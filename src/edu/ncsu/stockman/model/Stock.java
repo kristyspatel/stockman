d6252cef362afa6a232dec69628793c2000d6f98
Abdulrahman Alosaimy <abobander@macbook.local>
2014-02-11 19:22:38 -0500

    missed files from last push. Model, and Gati's Screens.

---
+++ src/edu/ncsu/stockman/model/Stock.java
@@ -0,0 +1,12 @@
+package edu.ncsu.stockman.model;
+
+public class Stock {
+
+	public Company company;
+	public int amount;
+	public Stock(Company company, int amount) {
+		super();
+		this.company = company;
+		this.amount = amount;
+	}
+}
---
+++ src/edu/ncsu/stockman/model/User.java
@@ -0,0 +1,40 @@
+package edu.ncsu.stockman.model;
+
+import java.util.ArrayList;
+
+import android.util.SparseArray;
+
+public class User {
+
+	public String email;
+	public String name;
+	public SparseArray<Game> games; 
+	public int id;
+	public SparseArray<User> friends;
+	public ArrayList<String> notifications;
+	
+	public User(String email, String name, int id) {
+		super();
+		this.email = email;
+		this.name = name;
+		this.id = id;
+		
+		this.games = new SparseArray<Game>();
+		this.friends = new SparseArray<User>();
+		this.notifications = new ArrayList<String>();
+	}
+	
+	public static boolean login(int id) // TODO change id to email and password
+	{
+		User u = Main.users.get(id);
+		if (u == null)
+			return false;
+		Main.current_user = u;
+		for (int i = 0; i < Main.games.size(); i++) {
+			Game g = Main.games.valueAt(i);
+			if (g.players.get(u.id)!=null)
+				u.games.put(g.id, g);
+		}
+		return true;
+	}
+}
