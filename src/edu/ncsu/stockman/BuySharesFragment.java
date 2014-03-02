74eea8ab35b99522db9a07e0fe5d68c2a25f0b7e
Kristy S Patel <patelkristy@yahoo.com>
2014-03-02 14:12:48 -0500

    RadioButton changes for buy shares

--- src/edu/ncsu/stockman/BuySharesFragment.java
+++ src/edu/ncsu/stockman/BuySharesFragment.java
@@ -1,5 +1,7 @@
 package edu.ncsu.stockman;
 
+import java.util.ArrayList;
+
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,6 +10,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class BuySharesFragment extends Fragment {
 
@@ -17,6 +21,8 @@ public class BuySharesFragment extends Fragment {
  
         View rootView = inflater.inflate(R.layout.buy_shares_fragment, container, false);
         ListView cardList = (ListView) rootView.findViewById(R.id.cardList);
+		//companies.add
+        BuySharesListAdapter.radioList = new ArrayList<RadioButton>();
 		String[] values = new String[] { "Maroon", "Blue", "green","yellow","violet","pink","red","purple"};
 		cardList.setAdapter(new BuySharesListAdapter(getActivity().getApplicationContext(),values));
         return rootView;
--- src/edu/ncsu/stockman/BuySharesListAdapter.java
+++ src/edu/ncsu/stockman/BuySharesListAdapter.java
@@ -1,16 +1,23 @@
 package edu.ncsu.stockman;
 
+import java.util.ArrayList;
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class BuySharesListAdapter extends ArrayAdapter<String> {
 	private final Context context;
 	private final String[] values;
+	static ArrayList<RadioButton> radioList = new ArrayList<RadioButton>();
+	 
 
 	public BuySharesListAdapter(Context context,String[] values) {
 		super(context, R.layout.buy_share_listitem, values);
@@ -23,6 +30,29 @@ public class BuySharesListAdapter extends ArrayAdapter<String> {
 		LayoutInflater inflater = (LayoutInflater) context
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		View rowView = inflater.inflate(R.layout.buy_share_listitem, parent, false);
+		
+		RadioButton cardRadio = (RadioButton) rowView.findViewById(R.id.radio_shares);
+		cardRadio.setTag(position);
+		radioList.add(cardRadio);
+		//cardRadio.set
+		cardRadio.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				//RadioButton r = (RadioButton) v;
+				for(RadioButton r: radioList){
+					if(r.getTag() != v.getTag())
+						r.setChecked(false);
+					else{
+						r.setChecked(true);
+					}
+					
+				}
+				
+				
+			}
+		});
 		TextView cardName = (TextView) rowView.findViewById(R.id.cardName);
 		cardName.setText(values[position]);
 		ImageView cardImage = (ImageView) rowView.findViewById(R.id.cardImage);
